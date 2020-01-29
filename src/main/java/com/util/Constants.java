cc68dff33c04922fe492747728d4fb442e20bf6b
Tsvetelin Kostadinov <tsvetelinkostadinovts@gmail.com>
2020-01-30 00:13:39 +0200

    added big decimal constants cuz i was bored

--- src/main/java/com/util/Constants.java
+++ src/main/java/com/util/Constants.java
@@ -6,6 +6,7 @@ package com.util;
 
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 import java.math.RoundingMode;
 
 
@@ -25,5 +26,45 @@ public abstract class Constants implements Serializable
 
     public static final RoundingMode ROUNDING_MODE  = RoundingMode.HALF_UP;
     
+    
+    public static enum BigDecimalNumbers
+    {
+        ZERO( BigDecimal.ZERO ),
+        ONE( BigDecimal.ONE ),
+        TWO( BigDecimal.ONE.add(BigDecimal.ONE) ),
+        THREE( TWO.getValue().add( ONE.getValue() ) ),
+        FOUR( THREE.getValue().add( ONE.getValue() ) ),
+        FIVE( FOUR.getValue().add( ONE.getValue() ) ),
+        SIX( FIVE.getValue().add( ONE.getValue() ) ),
+        SEVEN( SIX.getValue().add( ONE.getValue() ) ),
+        EIGHT( SEVEN.getValue().add( ONE.getValue() ) ),
+        NINE( EIGHT.getValue().add( ONE.getValue() ) ),
+        TEN( NINE.getValue().add( ONE.getValue() ) ),
+        ELEVEN( TEN.getValue().add( ONE.getValue() ) ),
+        TWELVE( ELEVEN.getValue().add( ONE.getValue() ) ),
+        THIRTEEN( TWELVE.getValue().add( ONE.getValue() ) ),
+        FOURTEEN( THIRTEEN.getValue().add( ONE.getValue() ) ),
+        FIFTEEN( FOURTEEN.getValue().add( ONE.getValue() ) ),
+        SIXTEEN( FIFTEEN.getValue().add( ONE.getValue() ) ),
+        SEVENTEEN( SIXTEEN.getValue().add( ONE.getValue() ) ),
+        EIGHTEEN( SEVENTEEN.getValue().add( ONE.getValue() ) ),
+        NINETEEN( EIGHTEEN.getValue().add( ONE.getValue() ) ),
+        TWENTY( NINETEEN.getValue().add( ONE.getValue() ) );
+        
+        private final BigDecimal val;
+        
+        BigDecimalNumbers( BigDecimal val)
+        {
+            this.val = val;
+        }
+
+        /**
+         * @return the val
+         */
+        public BigDecimal getValue ()
+        {
+            return val;
+        }
+    }
     
 }
